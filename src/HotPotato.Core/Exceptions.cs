1abfaeaa496886c9529cbb11047ee1b3bf49b507
Timothy Cosgriff <timothy.cosgriff@gmail.com>
2023-04-27 08:50:47 -0400

    Use a fallback controller action instead of a middleware. Works surprisingly well.

--- src/HotPotato.Core/Exceptions.cs
+++ src/HotPotato.Core/Exceptions.cs
@@ -9,6 +9,8 @@ namespace HotPotato.Core
 			new ArgumentNullException(paramName);
 		public static InvalidOperationException InvalidOperation(string message) =>
 			new InvalidOperationException(message);
+		public static InvalidOperationException MissingConfigKey(string key) =>
+			new InvalidOperationException($"'{key}' is not defined");
 		public static SpecNotFoundException SpecNotFound(string specLocation, HttpResponseMessage response) =>
 			new SpecNotFoundException(specLocation, response);
 	}
