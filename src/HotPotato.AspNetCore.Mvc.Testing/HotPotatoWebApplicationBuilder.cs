e1419c9a1fddef0b61cf24c732798142afe7e49c
Timothy Cosgriff <timothy.cosgriff@gmail.com>
2023-09-09 18:12:41 -0400

    Update csproj, getting initial test to operate

--- src/HotPotato.AspNetCore.Mvc.Testing/HotPotatoWebApplicationBuilder.cs
+++ src/HotPotato.AspNetCore.Mvc.Testing/HotPotatoWebApplicationBuilder.cs
@@ -4,20 +4,29 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace HotPotato.AspNetCore.Mvc.Testing;
 
 public class HotPotatoWebApplicationBuilder<TProgram>
 	: WebApplicationFactory<TProgram> where TProgram : class
 {
+	private readonly string specLocation;
+
+	public HotPotatoWebApplicationBuilder(string specLocation)
+	{
+		this.specLocation = specLocation ?? throw new ArgumentNullException(nameof(specLocation));
+	}
 
 	protected override void ConfigureWebHost(IWebHostBuilder builder)
 	{
 		builder.ConfigureServices(services =>
 		{
 			services.AddHotPotatoServices();
-			
+
 		});
+		builder.UseSetting("SpecLocation", this.specLocation);
 		builder.Configure(app =>
 		{
 			app.UseMiddleware<HotPotatoMiddleware>();
--- test/HotPotato.AspNetCore.Mvc.Testing.Test/UnitTest1.cs
+++ test/HotPotato.AspNetCore.Mvc.Testing.Test/UnitTest1.cs
@@ -2,18 +2,18 @@ namespace HotPotato.AspNetCore.Mvc.Testing;
 
 public class Tests
 {
+    private const string specLocation = "https://raw.githubusercontent.com/HylandSoftware/Hot-Potato/master/test/RawPotatoSpec.yaml";
     [SetUp]
     public void Setup()
     {
     }
 
     [Test]
-    public void Test1()
+    public async Task Test1()
     {
-        var builder = new HotPotatoWebApplicationBuilder<Test.Api.Startup>();
+        var builder = new HotPotatoWebApplicationBuilder<Test.Api.Startup>(specLocation);
         var client = builder.CreateClient();
-        var response = client.GetAsync("/orders");
-        
+        var response = await client.GetAsync("/orders");
+
     }
 }
-
